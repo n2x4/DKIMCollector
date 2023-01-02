1642bd7903fa651d50d180c03fc075092b053203
n2x4 <31667936+n2x4@users.noreply.github.com>
2023-01-01 21:16:30 -0500

    Update pydnstest.py

--- pydnstest.py
+++ pydnstest.py
@@ -7,4 +7,12 @@ dkimdomain = "gmail.com"
 dnsquery = pydig.query(selector + "._domainkey." + dkimdomain, 'TXT')
 print(dnsquery)
 
-dnsquery2 = dns.resolver.resolve(selector + "._domainkey." + dkimdomain, 'TXT')
+print()
+print ("Testing domain", dkimdomain, "for DKIM record with selector", selector, "...")
+try:
+  test_dkim = dns.resolver.resolve(selector + '._domainkey.' + dkimdomain , 'TXT')
+  for dns_data in test_dkim:
+    if 'DKIM1' in str(dns_data):
+      print ("  [PASS] DKIM record found  :",dns_data)
+except:
+  print ("  [FAIL] DKIM record not found.")
